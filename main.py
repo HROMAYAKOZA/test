e0a62d473684c86fa973e7fd76705a17ff55a6d0
Leonid <Leonid.shahmetov3000@yandex.ru>
2023-09-03 21:42:24 +0300

    new func

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import json
 f = open('text.txt', 'r')
 print(*f)
 f.close()
 
 f = open('secretcode.txt', 'r')
-print("Your code is: %s" % f.read())
+s = f.read()
+user = input("Your username: ")
+with open('database.json', 'r') as file:
+    data = json.load(file)
+    password = data.get(user, None)
+if password == s:
+    print("Success")
+else:
+    print("You don't have access")
+print("Your code is: %s" % password)
 f.close
